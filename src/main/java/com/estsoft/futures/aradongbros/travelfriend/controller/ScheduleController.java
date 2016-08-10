0c5392bafd1394b9a47c5dbe5088bd5e709a7269
choiyoungjin <tyranosaurus@nate.com>
2016-08-11 01:16:36 +0900

    스케줄 테이블 - isfinished, start/endDate 수정

--- src/main/java/com/estsoft/futures/aradongbros/travelfriend/controller/ScheduleController.java
+++ src/main/java/com/estsoft/futures/aradongbros/travelfriend/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.estsoft.futures.aradongbros.travelfriend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,9 +18,8 @@ public class ScheduleController
 	
 	// 조회
 	
-	// 삽입
+	// 삽입  -> url : localhost:8080/TravelFriendAndroid/schedule/schInsert
 	@RequestMapping("/schInsert")
-	//url : localhost:8080/TravelFriendAndroid/schedule/schInsert
 	public void insertScheduleData(@RequestBody ScheduleVo schVo)
 	{	
 		if ( schVo.getIsfinished() == "0" )
@@ -31,10 +31,17 @@ public class ScheduleController
 			schVo.setIsfinished("finished");
 		}
 		
+		schVo.getStartDate().replaceAll("/", "-");
+		schVo.getEndDate().replaceAll("/", "-");
+		
 		scheduleService.insertScheduleData(schVo);
 	}
 	
-	// 삭제
-	
+	// 삭제 -> url : localhost:8080/TravelFriendAndroid/schedule/schDelete/{no}
+	@RequestMapping("/schDelete/{no}")
+	public void deleteScheduleData(@PathVariable("no") int no)
+	{			
+		//scheduleService.deleteScheduleData(no);
+	}
 	// 수정
 }
