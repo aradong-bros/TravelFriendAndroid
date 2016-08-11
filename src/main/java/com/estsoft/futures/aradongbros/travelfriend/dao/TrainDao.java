52cbc097fab4d4ebde5205b0281fad1547668176
kisay97 <kisay97@naver.com>
2016-08-11 21:51:27 +0900

    기차 시간표 조회(직통) 완료

--- src/main/java/com/estsoft/futures/aradongbros/travelfriend/dao/TrainDao.java
+++ src/main/java/com/estsoft/futures/aradongbros/travelfriend/dao/TrainDao.java
@@ -1,5 +1,54 @@
 package com.estsoft.futures.aradongbros.travelfriend.dao;
 
-public class TrainDao {
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.estsoft.futures.aradongbros.travelfriend.vo.TrainInfoVo;
+import com.estsoft.futures.aradongbros.travelfriend.vo.TrainOperationRouteVo;
+
+@Repository
+public class TrainDao 
+{
+	@Autowired
+	private SqlSession sqlSession;
+
+	public Integer getStationNo(String stationName) 
+	{
+		Integer no = sqlSession.selectOne("train.selectStationNo", stationName);
+		return no;
+	}
+
+	public TrainInfoVo selectTrainInfoByNo(int no) 
+	{
+		TrainInfoVo trainInfoVo = sqlSession.selectOne("train.selectTrainInfoByNo", no);
+		return trainInfoVo;
+	}
+
+	public List<TrainOperationRouteVo> getStartTrainTimeList(int startStationNo, int endStationNo, int categoryNo, Time goTime) 
+	{
+		Map<String, Object> map = new HashMap<>();
+		map.put("startStationNo", startStationNo);
+		map.put("endStationNo", endStationNo);
+		map.put("categoryNo", categoryNo);
+		map.put("goTime", goTime);
+		List<TrainOperationRouteVo> trainTimeList = sqlSession.selectList("train.selectStartTrainTime", map);
+		return trainTimeList;
+	}
+	
+	public List<TrainOperationRouteVo> getEndTrainTimeList(int startStationNo, int endStationNo, int categoryNo, Time goTime) 
+	{
+		Map<String, Object> map = new HashMap<>();
+		map.put("startStationNo", startStationNo);
+		map.put("endStationNo", endStationNo);
+		map.put("categoryNo", categoryNo);
+		map.put("goTime", goTime);
+		List<TrainOperationRouteVo> trainTimeList = sqlSession.selectList("train.selectENdTrainTime", map);
+		return trainTimeList;
+	}
 }
