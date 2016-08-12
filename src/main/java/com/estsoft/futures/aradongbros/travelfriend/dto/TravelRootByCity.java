c365f5a0e90e73acaca12328aabf3051c16685db
choiyoungjin <tyranosaurus@nate.com>
2016-08-12 16:52:34 +0900

    스케줄 경로 수정

---
+++ src/main/java/com/estsoft/futures/aradongbros/travelfriend/dto/TravelRootByCity.java
@@ -0,0 +1,21 @@
+package com.estsoft.futures.aradongbros.travelfriend.dto;
+
+import java.util.Arrays;
+
+public class TravelRootByCity 
+{
+	int[] TRAVEL_ROOT;
+
+	public int[] getTRAVEL_ROOT() {
+		return TRAVEL_ROOT;
+	}
+
+	public void setTRAVEL_ROOT(int[] tRAVEL_ROOT) {
+		TRAVEL_ROOT = tRAVEL_ROOT;
+	}
+
+	@Override
+	public String toString() {
+		return "TravelRootByCity [TRAVEL_ROOT=" + Arrays.toString(TRAVEL_ROOT) + "]";
+	}
+}
